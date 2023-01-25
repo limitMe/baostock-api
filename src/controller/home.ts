070a0ecb93dad91b4d9beba86c04357a3c2bd9f9
limitMe <z@limme.net>
2023-01-26 00:04:18 +0800

    Add baostock example

--- src/controller/home.ts
+++ src/controller/home.ts
@@ -1,8 +1,14 @@
 import { Context } from "koa";
-import { runPythonString } from '../helper/pythonShell'
+import { Options } from 'python-shell';
+import { runPythonString, runPythonFile } from '../helper/pythonShell'
 
 export async function main(ctx: Context) {
-  const output = await runPythonString("print('Hello World')", undefined)
+  const options = {
+    pythonOptions: ['-u'], // get print results in real-time
+    scriptPath: '/Users/zhongdian/Desktop/Dev/baostock-api/python',
+    args: []
+  };
+  const output = await runPythonFile('daily.py', options)
   console.log(output)
   ctx.body = {
     "key": "value"
